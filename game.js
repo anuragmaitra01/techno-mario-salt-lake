c3291398c16d39c7cd0a37d416212c28e9c7f522
Nilesh Sengupta <nileshsengupta43@gmail.com>
2023-04-21 18:19:05 +0530

    Update game.js

--- game.js
+++ game.js
@@ -16,8 +16,8 @@ let isJumping = true
 
 
 loadSprite('mario', 'https://i.ibb.co/cDXP5SN/mario.png');
-loadSprite('mario2', 'https://i.ibb.co/cDXP5SN/mario.png');
-loadSprite('mario3', 'https://i.ibb.co/cDXP5SN/mario.png');
+loadSprite('mario2', 'https://i.ibb.co/n7z6p7Q/New-Piskel-1.png');
+loadSprite('mario3', 'https://i.ibb.co/YN9ZtS0/New-Piskel.png');
 
 loadSprite('coin', 'https://i.ibb.co/dgP4nLk/cake.png')
 loadSprite('sky', 'https://i.ibb.co/ZLB1GNM/images.jpg');
@@ -38,6 +38,59 @@ loadSprite('blue-brick', 'https://i.imgur.com/3e5YRQd.png')
 loadSprite('blue-steel', 'https://i.imgur.com/gqVoI2b.png')
 loadSprite('blue-evil-shroom', 'https://i.ibb.co/9n7CSB6/choco-cake.png')
 loadSprite('blue-surprise', 'https://i.imgur.com/RMqCc1G.png')
+
+scene('startScreen', () => {
+  add([
+    text('Welcome to "Super Student Bros: The Corona Years"', 13),
+    origin('center'),
+    pos(width()/2, height()/2 -140),
+    color(1, 0, 0)
+  ]);
+
+  const textLines = [
+    'A browser-based game that is a hilarious parody on the life of a student in Techno Main Salt Lake.',
+    'In this game, you\'ll take control of your favorite student character ',
+    'and navigate through the challenges of remote learning during the pandemic.',
+    '',
+    'Instead of the usual Goombas, you\'ll be battling the dreaded Corona Virus Goombas that are out to get you.',
+    'And instead of collecting coins, you\'ll be collecting those pesky worksheets that your teachers keep assigning.',
+    'But fear not, dear student, for there are hidden easter eggs ',
+    'in the form of mushrooms that will help you on your journey.',
+    '',
+    'These mushrooms will give you the much-needed energy to keep going,',
+    'just like how your favorite energy drink helps you survive your never-ending classes.',
+    'As you progress through the game, you\'ll encounter obstacles such as unstable Wi-Fi connections,',
+    'annoying classmates who won\'t stop messaging you on WhatsApp,',
+    'and those dreaded online quizzes that never seem to end. But don\'t worry, we know you can conquer them all!',
+    '',
+    'So put on your gaming hat and get ready to experience the life',
+    'of a Techno Main Salt Lake student like never before.',
+    'Remember, the key to success in this game is to be as sarcastic and funny as possible,',
+    'just like your favorite memes that keep you going through those boring lectures.',
+    'Let the fun begin!'
+    ];
+    
+    textLines.forEach((line, index) => {
+    add([
+    text(line, 6),
+    origin('center'),
+    pos(width() / 2, height() / 2 - 100+ (index * 10)),
+    color(0.678, 0.847, 0.902)
+    ]);
+    });
+
+  add([
+    text('Press Enter to select a character', 16),
+    origin('center'),
+    pos(width() / 2, height() / 2 + 120),
+    color(0, 1, 0)
+  ]);
+
+  keyPress('enter', () => {
+    go('characterSelect');
+  });
+});
+
 
 
 scene('characterSelect', () => {
@@ -304,4 +357,4 @@ scene('lose', ({ score }) => {
 });
 
 
-start("characterSelect");
+start("startScreen");
